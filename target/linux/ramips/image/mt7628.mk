6886fce66883d85a64c445fc6416124f41ae0ed4
JamesT42 <j.tuvix@gmx.de>
2017-06-06 21:05:57 +0200

    added tftp image

--- target/linux/ramips/image/mt7628.mk
+++ target/linux/ramips/image/mt7628.mk
@@ -39,14 +39,14 @@ define Device/tl-wr841n-v13
   DTS := TL-WR841NV13
   IMAGE_SIZE := 7808k
   DEVICE_TITLE := TP-Link TL-WR841N v13
-  SUPPORTED_DEVICES := tl-wr841nv13    # what to write here? tl-wr841nv13?
+  SUPPORTED_DEVICES := tl-wr841n-v13
   DEVICE_PACKAGES :=
-  KERNEL := $(KERNEL_DTB)
-  KERNEL_INITRAMFS := $(KERNEL_DTB) | tplink-header TL-WR841Nv13 -c   # untested
-  IMAGES += factory.bin factory2.bin
-  IMAGE/factory.bin := pad-extra 131072 | $$(sysupgrade_bin)
-  IMAGE/factory2.bin := append-kernel | tplink-header TL-WR841Nv13 -j
+  KERNEL := $(KERNEL_DTB) | prepend-to 448 | uImage lzma
+  # KERNEL_INITRAMFS := $(KERNEL_DTB) | tplink-header TL-WR841Nv13 -c        # untested
+  IMAGES += factory.bin tftp.bin
+  IMAGE/factory.bin := pad-extra 131584 | append-kernel | tplink-header TL-WR841Nv13 -j
   IMAGE/sysupgrade.bin := append-kernel | tplink-header TL-WR841Nv13 -j -s | append-metadata
+  IMAGE/tftp.bin := pad-extra 131072 | $$(sysupgrade_bin)
 endef
 TARGET_DEVICES += tl-wr841n-v13
 
